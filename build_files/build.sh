7b2525c9511150b633c51d3c7f8ba6c8a4cfe785
vhalragnarok-ffxiv <164896093+vhalragnarok-ffxiv@users.noreply.github.com>
2026-09-20 16:41:09 -0500

    Update build.sh: Jetbrains Toolbox

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,6 +22,24 @@ install -Dm0644 \
     /ctx/system_files/usr/share/applications/unityhub.desktop \
     /usr/share/applications/unityhub.desktop
 
+#Jetbrains Toolbox!
+# Install latest JetBrains Toolbox
+mkdir -p /opt/jetbrains-toolbox
+
+curl -fL --retry 3 \
+    "https://data.services.jetbrains.com/products/download?code=TBA&platform=linux" \
+    -o /tmp/jetbrains-toolbox.tar.gz
+
+tar -xzf /tmp/jetbrains-toolbox.tar.gz \
+    -C /opt/jetbrains-toolbox \
+    --strip-components=1
+
+rm -f /tmp/jetbrains-toolbox.tar.gz
+
+ln -sf \
+    /opt/jetbrains-toolbox/bin/jetbrains-toolbox \
+    /usr/local/bin/jetbrains-toolbox
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
