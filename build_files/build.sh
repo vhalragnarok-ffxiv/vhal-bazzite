ca5d8600fbbdf6688c591d6376b604351b857e6a
vhalragnarok-ffxiv <164896093+vhalragnarok-ffxiv@users.noreply.github.com>
2026-09-20 16:28:29 -0500

    Update build.sh: Condensed Unity & .NET SDK to 1 line.

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,11 +12,8 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y dotnet-sdk-10.0
-
-#Install Unity 
-dnf5 install -y unityhub
+# Install Unity & .NET SDK
+dnf5 install -y unityhub dotnet-sdk-10.0
 
 # Use a COPR Example:
 #
