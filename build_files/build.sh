77dc12389874670c845922872cce74b800e5cb1c
vhalragnarok-ffxiv <164896093+vhalragnarok-ffxiv@users.noreply.github.com>
2026-09-20 16:26:12 -0500

    Update build.sh : Add dotnet-sdk

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-
+dnf5 install -y dotnet-sdk-10.0
 
 #Install Unity 
 dnf5 install -y unityhub
